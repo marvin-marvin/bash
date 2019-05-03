4182ff7f56b8e351fde301e30117ebe25ae8c21f
Marvin <25531193+marvin-marvin@users.noreply.github.com>
2019-05-03 15:38:32 +0200

    fix symlink

--- prepare.sh
+++ prepare.sh
@@ -4,8 +4,8 @@ mv ~/.bash* .bash_old
 mv ~/.ssh .bash_old
 
 # prepare bash
-ln -s ~/bash-acc/.bash
-ln -s ~/bash-acc/.bashrc
-ln -s ~/bash-acc/.bash_profile
-ln -s ~/bash-acc/.ssh
-ln -s ~/bash-acc/keys
+ln -s ~/bash-acc/.bash ~/.bash
+ln -s ~/bash-acc/.bashrc ~/.bashrc
+ln -s ~/bash-acc/.bash_profile ~/.bash_profile
+ln -s ~/bash-acc/.ssh ~/.ssh
+ln -s ~/bash-acc/keys ~/keys
