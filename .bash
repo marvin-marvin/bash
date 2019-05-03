5ade44f349ab652828f5b2026725ed38a458ab4d
Marvin <marvin-marvin@users.noreply.github.com>
2019-05-03 21:15:18 +0200

    fix

--- .bash
+++ .bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-exec /bin/bash --rcfile ~/.bashrc
+if [[ "$HOSTNAME" == "vps-ger-fra-1" ]] ; then
+    exec /bin/bash --rcfile ~/.bashrc
+else
+    exec /bin/bash --rcfile /tmp/.bashrc
+fi
+
 exit 0
