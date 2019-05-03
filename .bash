e3c7f65a255dae8b9134955b3efc7f9ce0225044
Marvin <25531193+marvin-marvin@users.noreply.github.com>
2019-05-03 15:24:22 +0200

    prepare bash

--- .bash
+++ .bash
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-ln -s ~/bash-acc/.bash
-ln -s ~/bash-acc/.bashrc
-ln -s ~/bash-acc/.bash_profile
-ln -s ~/bash-acc/.ssh
-ln -s ~/bash-acc/keys
+
 exec /bin/bash --rcfile ~/.bashrc
 exit 0
---
+++ ln.sh
@@ -0,0 +1,6 @@
+# prepare bash
+ln -s ~/bash-acc/.bash
+ln -s ~/bash-acc/.bashrc
+ln -s ~/bash-acc/.bash_profile
+ln -s ~/bash-acc/.ssh
+ln -s ~/bash-acc/keys
