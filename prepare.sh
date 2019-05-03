4dac1c54e965d0d04698044e3ef5fc942fad5233
Marvin <25531193+marvin-marvin@users.noreply.github.com>
2019-05-03 15:40:47 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,10 @@
 # backup existing files
 mkdir ~/.bash_old
-mv ~/.bash* .bash_old
-mv ~/.ssh .bash_old
+mv ~/.bash* ~/.bash_old
+mv ~/.ssh ~/.bash_old
 
 # prepare bash
 ln -s ~/bash-acc/.bash ~/.bash
 ln -s ~/bash-acc/.bashrc ~/.bashrc
 ln -s ~/bash-acc/.bash_profile ~/.bash_profile
 ln -s ~/bash-acc/.ssh ~/.ssh
-ln -s ~/bash-acc/keys ~/keys
