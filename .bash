b6012a2e97ed44d07b671e0afa15a52b307b3fc7
Marvin <25531193+marvin-marvin@users.noreply.github.com>
2019-05-03 13:49:09 +0200

    cleanup

--- .bash
+++ .bash
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-exec /bin/bash --rcfile /root/bash-acc/.bashrc
+ln -s ~/bash-acc/.bash
+ln -s ~/bash-acc/.bashrc
+ln -s ~/bash-acc/.bash_profile
+ln -s ~/bash-acc/.ssh
+ln -s ~/bash-acc/keys
+exec /bin/bash --rcfile ~/.bashrc
 exit 0
