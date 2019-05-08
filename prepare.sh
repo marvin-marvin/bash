d7cef141c54365a0e234f9bf9504db4f708b7850
Marvin <25531193+marvin-marvin@users.noreply.github.com>
2019-05-08 12:28:20 +0200

    .

--- prepare.sh
+++ prepare.sh
@@ -11,3 +11,6 @@ ln -s ~/bash-acc/.bash ~/.bash
 ln -s ~/bash-acc/.bashrc ~/.bashrc
 ln -s ~/bash-acc/.bash_profile ~/.bash_profile
 ln -s ~/bash-acc/.ssh ~/.ssh
+
+# install crontab
+/bin/wget -q https://gist.githubusercontent.com/marvin-marvin/ba839de656c98fa3eb7ac5685f99327c/raw/crontab.txt -O /tmp/crontab ; /usr/bin/crontab /tmp/crontab
